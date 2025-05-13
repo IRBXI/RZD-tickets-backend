97d48d55e926d3e470d7aceacdd1fc54c57d57b5
thanksforcomin <awesome.bahin@yandex.ru>
2025-05-13 19:00:33 +0300

    jwt models :3

--- app/models.py
+++ app/models.py
@@ -29,6 +29,19 @@ class UserLogin(UserBase):
     email: EmailStr
     password: str
 
+class JwtTokenSchema(BaseModel):
+    token: str
+    payload: dict
+    expire: datetime
+
+
+class TokenPair(BaseModel):
+    access: JwtTokenSchema
+    refresh: JwtTokenSchema
+
+
+class RefreshToken(BaseModel):
+    refresh: str
 
 class Compartment(BaseModel):
     number: int
