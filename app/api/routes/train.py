7a85ccf2d260580050ec8b13cf2cd6bab94d55df
IRBXI <rbxrbx@mail.ru>
2025-05-13 17:01:42 +0300

    fixed url building

--- app/api/routes/train.py
+++ app/api/routes/train.py
@@ -9,10 +9,14 @@ from http import HTTPStatus
 DEFAULT_URL = "https://pass.rzd.ru"
 GET_TRAINS_URL = build_url(
     "https://pass.rzd.ru/timetable/public/ru",
-    {"layer_id": "5827", "dir": "0", "tfl": "3", "checkSeats": "1", "md": "0"},
+    layer_id=5827,
+    dir=0,
+    tfl=3,
+    checkSeats=1,
+    md=0,
 )
 STATIONS_URL = build_url(
-    "https://pass.rzd.ru/ticket/services/route/basicRoute", {"STRUCTURE_ID": "5418"}
+    "https://pass.rzd.ru/ticket/services/route/basicRoute", STRUCTURE_ID=5418
 )
 
 
@@ -24,7 +28,7 @@ async def setup_cookies() -> None:
     await client.get(DEFAULT_URL)
 
 
-async def rzd_post(url: str) -> dict:
+async def rzd_post(url: str):
     r = await client.post(url)
     if r.status_code != HTTPStatus.OK or r.json()["result"] == "FAIL":
         return {"error": "Couldn't get the response from rzd"}
@@ -47,11 +51,9 @@ async def get_train(
 ):
     url = build_url(
         GET_TRAINS_URL,
-        {
-            "code0": request_data.from_code,
-            "code1": request_data.to_code,
-            "dt0": request_data.date,
-        },
+        code0=request_data.from_code,
+        code1=request_data.to_code,
+        dt0=request_data.date,
     )
 
     response_data = await rzd_post(url)
@@ -68,10 +70,8 @@ async def get_train_seats(
 ):
     url = build_url(
         STATIONS_URL,
-        {
-            "trainNumber": request_data.train_number,
-            "depDate": request_data.train_request.date,
-        },
+        trainNumber=request_data.train_number,
+        depDate=request_data.train_request.date,
     )
 
     response_data = await rzd_post(url)
