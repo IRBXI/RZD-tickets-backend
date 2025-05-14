c638cc346a45abcd629a6b34aea56fceee30162c
thanksforcomin <awesome.bahin@yandex.ru>
2025-05-14 23:28:02 +0300

    added the db models for auth

--- app/db_models.py
+++ app/db_models.py
@@ -1,6 +1,29 @@
 from tortoise.models import Model
 from tortoise import fields
 
+class BaseModel(Model):
+    id = fields.IntField(primary_key=True)
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    async def get_by_id(cls, record_id: int):
+        return await cls.get_or_none(id=record_id)
+
+class User(BaseModel):
+    name = fields.CharField(max_length=255)
+    email = fields.CharField(max_length=255, unique=True)
+
+    class Meta:
+        table = "users"
+
+    @classmethod
+    async def find_by_email(cls, email: str):
+        return await cls.get_or_none(email=email)
+
+    @classmethod
+    async def validate()
 
 class Station(Model):
     # Stations use 7 character long ids
