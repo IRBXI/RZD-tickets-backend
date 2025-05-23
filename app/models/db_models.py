094914f130a75ea8f16b50e0c70a1597f8bcd802
IRBXI <rbxrbx@mail.ru>
2025-05-24 00:16:05 +0300

    fixed db model

--- app/models/db_models.py
+++ app/models/db_models.py
@@ -18,6 +18,7 @@ class Station(Model):
     # All stations use 7 character long ids
     id = fields.CharField(primary_key=True, max_length=7)
     # TODO: Maybe research all the posible station names to cut down on the max_length
+    name = fields.CharField(max_length=100)
 
 
 class ModelBase(Model):
