b75f531382c2069ed57bf67059d84d4d1f2384b9
IRBXI <rbxrbx@mail.ru>
2025-05-23 22:33:16 +0300

    renamed one file

--- app/api/main.py
+++ app/api/main.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, train, stations
+from app.api.routes import login, trains, stations
 
 api_router = APIRouter()
 
 api_router.include_router(login.router)
-api_router.include_router(train.router)
+api_router.include_router(trains.router)
 api_router.include_router(stations.router)
